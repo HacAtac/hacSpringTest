650b31906d254731447a9338cd8a60a04ff4effe
Jordan Hackworth <jhackworth00@outlook.com>
2022-11-26 23:51:10 -0600

    GET rest API using @PathVariable + @RequestParam anotations

--- springboot-rest-api/src/main/java/net/hacatac/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/net/hacatac/springboot/controller/StudentController.java
@@ -3,6 +3,7 @@ package net.hacatac.springboot.controller;
 import net.hacatac.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -45,4 +46,24 @@ public class StudentController {
         );
         return student;
     }
+
+    //http://localhost:8080/students/1/ramesh/fadatare
+    //{id} - URI Template Variable
+    //{firstName} - URI Template Variable
+    //{lastName} - URI Template Variable
+    @GetMapping("/students/{id}/{first-name}/{last-name}")
+    public Student studentPathVariable2(@PathVariable("id") int studentId,
+                                        @PathVariable("first-name") String firstName,
+                                        @PathVariable("last-name") String lastName) {
+        return new Student(studentId, firstName, lastName);
+    }
+
+    //Spring boot REST API with RequestParam
+    //http://localhost:8080/students/query?id=1&firstName=Jordan&lastName=Henderson
+    @GetMapping("/students/query")
+    public Student studentRequestVariable(@RequestParam int id, @RequestParam String firstName, @RequestParam String lastName) {
+        return new Student(id, firstName, lastName);
+    }
 }
+
+
